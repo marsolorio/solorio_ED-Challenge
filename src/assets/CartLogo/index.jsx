309db98e388c33fd455c254e19764ad80a4a9d7b
Martin Solorio <msolorio@berkeley.edu>
2024-05-14 14:15:18 -0700

    Tested again

--- src/assets/CartLogo/index.jsx
+++ src/assets/CartLogo/index.jsx
@@ -3,6 +3,7 @@ import './CartLogo.css'
 function CartLogo () {
     return (
         <div className='cartLogoContainer'>
+            <p>Hello world</p>
             <img src='src/assets/CartLogo/shopping-cart.png'/>
         </div>
     )
